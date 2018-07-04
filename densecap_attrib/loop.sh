f107906f9c87b0c14c99f91b09c6223c1855d06a
ikhurram29 <40542419+ikhurram29@users.noreply.github.com>
2018-07-05 00:06:01 +0500

    Add files via upload

---
+++ densecap_attrib/loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for number in {1..82783}
+do
+th run_model.lua -input_dir /home/imrankhurram/Downloads/train2014 -max_images 1
+done
+exit 0
+
+the woman is standing with her luggage
+best score: 1.0-best caption: a baby is holding and eating a banana
